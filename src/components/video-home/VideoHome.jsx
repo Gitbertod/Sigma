7487a66ff692906a5d5fc2205e13e26fb63b167e
Gilberto Díaz <diazp.gilberto@gmail.com>
2024-11-11 15:54:08 -0500

    trying to resolve problem with video of home

--- src/components/video-home/VideoHome.jsx
+++ src/components/video-home/VideoHome.jsx
@@ -1,14 +1,20 @@
 import bgvideo2 from "../../assets/homeVideo.mov";
 import styles from "./VideoHome.module.css";
 
-
 const VideoHome = () => {
   return (
     <div className={styles.main}>
-        <div className={styles.overlay}></div>
-        <video src={bgvideo2} autoPlay loop muted className={styles.video}></video>
+      <div className={styles.overlay}></div>
+      <video
+        src={bgvideo2}
+        autoPlay
+        loop
+        muted
+        playsInline 
+        className={styles.video}
+      ></video>
     </div>
-  )
-}
+  );
+};
 
-export default VideoHome
+export default VideoHome;
--- src/views/news/News.jsx
+++ src/views/news/News.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import NavBar from "../../components/navbar/NavBar";
 
 const News = () => {
   return (
-    <div>News</div>
-  )
-}
+    <>
+      <NavBar/>
+      
+    </>
+  );
+};
 
-export default News
+export default News;

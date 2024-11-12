57756580adafc8416590066fd382ca4d5f58ae05
Gilberto Díaz <diazp.gilberto@gmail.com>
2024-11-12 15:44:16 -0500

    Busisness category done

--- src/views/advantages/Advantages.jsx
+++ src/views/advantages/Advantages.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import NavBar from "../../components/navbar/NavBar";
+import CategoryWithBackgroundVideo from "../../components/category/CategoryWithBackgroundVideo";
+import video_bg from "../../assets/construccion.mov";
+import styles from "./Advantages.module.css";
 
 const Advantages = () => {
   return (
-    <div>Advantages</div>
-  )
-}
+    <>
+      <NavBar />
+      <CategoryWithBackgroundVideo
+        title="Advantages: Operating Lease "
+        videoSrc={video_bg}
+        text={
+          <>
+            <ul className={styles.lista}>
+              <li>
+                <strong>OPEX Efficiency: </strong> The monthly fee is classified
+                as a current operating expense, allowing for optimal budget
+                management at 100%.
+              </li>
+              <br></br>
+              <li>
+                <strong>Put Option (Optional): </strong>
+                Sigma provides the option to sell assets to the customer at
+                approximately market value at the lease's conclusion, offering
+                flexibility and potential cost-effectiveness.
+              </li>
+              <br></br>
+              <li>
+                <strong>Specialized Maintenance:</strong> Direct (dealer) and
+                web indirect (dealer certified) maintenance services are
+                provided.
+              </li>
+              <br></br>
+              <li>
+                <strong>Periodic Machinery Renewal:</strong> The lessee has the
+                opportunity to renew its machinery periodically, ensuring access
+                to up-to-date and efficient equipment.
+              </li>
+              <br></br>
+              <li>
+                <strong>Outsourced Equipment Maintenance: </strong> Maintenance
+                is streamlined through a repair and maintenance contract with
+                the dealer, relieving the lessee of the responsibility and
+                ensuring specialized care for the leased assets.
+              </li>
+            </ul>
+          </>
+        }
+      />
+    </>
+  );
+};
 
-export default Advantages
+export default Advantages;
--- src/views/infrastructure-private-equity-managed-funds/InfrastructurePrivateEquityMF.jsx
+++ src/views/infrastructure-private-equity-managed-funds/InfrastructurePrivateEquityMF.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import NavBar from "../../components/navbar/NavBar";
+import CategoryWithBackgroundVideo from "../../components/category/CategoryWithBackgroundVideo";
+import styles from "./InfrastructurePrivateEquityMF.module.css";
+import video_bg from "../../assets/callaoPeru.mov";
 
 const InfrastructurePrivateEquityMF = () => {
   return (
-    <div>InfrasttructurePrivateEquityManagedFunds</div>
-  )
-}
+    <>
+      <NavBar />
+      <CategoryWithBackgroundVideo
+        videoSrc={video_bg}
+        title={"Managed Funds"}
+        text={
+          <>
+            <h2 className={styles.subtitle}>
+              Infrastructure Private Equity Fund
+            </h2>
+            <ul>
+              <li className={styles.lista}>
+                Launched in 2012, the $500 million private equity fund secured
+                $250 million in committed capital after two successful
+                fundraising rounds.
+              </li>
+              <li className={styles.lista}>
+                Investors include the three main Peruvian Pension Funds and two
+                other renowned institutional investors.
+              </li>
+              <li className={styles.lista}>
+                Dedicated to investing in greenfield infrastructure projects
+                encompassing power generation, transmission, transportation,
+                ports, etc. Sigma collaborates with key global sponsors to
+                develop impactful projects in Peru.
+              </li>
+            </ul>
+          </>
+        }
+      />
+    </>
+  );
+};
 
 export default InfrastructurePrivateEquityMF;

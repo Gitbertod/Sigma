00d410ded825f77c12dfca94289ac744433ff844
Gilberto Díaz <diazp.gilberto@gmail.com>
2025-03-14 11:46:00 -0500

    fixed error in advantages view

--- src/views/advantages/Advantages.jsx
+++ src/views/advantages/Advantages.jsx
@@ -1,49 +1,40 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import NavBar from "../../components/navbar/NavBar";
 import CategoryWithBackgroundVideo from "../../components/category/CategoryWithBackgroundVideo";
 import video_bg from "../../assets/ventajas.mp4";
 import styles from "./Advantages.module.css";
 
 const Advantages = () => {
+  const { t } = useTranslation();
   return (
     <>
       <NavBar />
       <CategoryWithBackgroundVideo
-        title="Advantages: Operating Lease "
+        title={t("Advantages.Title")}
         videoSrc={video_bg}
         text={
           <>
             <ul className={styles.lista}>
               <li>
-                <strong>OPEX Efficiency: </strong> The monthly fee is classified
-                as a current operating expense, allowing for optimal budget
-                management at 100%.
+                <strong>{t("Advantages.subTilte1")}</strong> {t("Advantages.Paragraph1")}
               </li>
               <br></br>
               <li>
-                <strong>Put Option (Optional): </strong>
-                Sigma provides the option to sell assets to the customer at
-                approximately market value at the lease's conclusion, offering
-                flexibility and potential cost-effectiveness.
+                <strong>{t("Advantages.subTilte2")}</strong>
+                {t("Advantages.Paragraph2")}
               </li>
               <br></br>
               <li>
-                <strong>Specialized Maintenance:</strong> Direct (dealer) and
-                web indirect (dealer certified) maintenance services are
-                provided.
+                <strong>{t("Advantages.subTilte3")}</strong>{t("Advantages.Paragraph3")}
               </li>
               <br></br>
               <li>
-                <strong>Periodic Machinery Renewal:</strong> The lessee has the
-                opportunity to renew its machinery periodically, ensuring access
-                to up-to-date and efficient equipment.
+                <strong>{t("Advantages.subTilte4")}</strong> {t("Advantages.Paragraph4")}
               </li>
               <br></br>
               <li>
-                <strong>Outsourced Equipment Maintenance: </strong> Maintenance
-                is streamlined through a repair and maintenance contract with
-                the dealer, relieving the lessee of the responsibility and
-                ensuring specialized care for the leased assets.
+                <strong>{t("Advantages.subTilte5")} </strong> {t("Advantages.Paragraph5")}
               </li>
             </ul>
           </>
